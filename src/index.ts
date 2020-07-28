fce5150b6f94e39186af1c581ffcbce02112a1f4
Thomas Allen <thomas@bluecanvas.io>
2020-07-28 12:12:05 -0700

    dist: Export enums, include types dir.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,12 @@ import EventHandlerPlugin from './hapi/EventHandlerPlugin';
 
 export { Client } from './client';
 export { EventHandlerPlugin };
+
+export {
+  DeploymentType,
+  DeploymentState,
+  DeploymentPhase,
+  TestLevel,
+  CheckState,
+  CheckResult,
+} from './types';
