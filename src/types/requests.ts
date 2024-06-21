aa6def524cefce3b7e126361302c637580415905
Reen Lokum <reen@bluecanvas.io>
2024-06-21 17:36:55 +0200

    fix: Update and reduce dependencies

--- src/types/requests.ts
+++ src/types/requests.ts
@@ -26,6 +26,27 @@ export interface DeploymentsPutCheckResponse {
   check: DeploymentCheck;
 }
 
+export interface DeploymentsDeleteCheckRequest {
+  /**
+   * The numeric identifier of the Deployment Request for which you are
+   * updating the check.
+   */
+  deploymentNumber: number;
+
+  /**
+   * The canonical identifier of the status check, for example
+   * `"code-coverage"`.
+   */
+  name: string;
+}
+
+export interface DeploymentsDeleteCheckResponse {
+  /**
+   * The properties of the deleted check.
+   */
+  check: DeploymentCheck;
+}
+
 export interface ArchivesGetTarGzipBlobRequest {
   /**
    * The Git revision to archive, see `man gitrevision`.
